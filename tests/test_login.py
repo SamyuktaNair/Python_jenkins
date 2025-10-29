2f18d79fb29e3964ef4b5906e555128846c07400
SamyuktaNair <nsamyukta11@gmail.com>
2025-10-29 23:13:06 +0530

    small changes

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,8 +6,5 @@ def test_google_search():
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
     assert "Google" in driver.title
-    search_box = driver.find_element(By.NAME, "q")
-    search_box.send_keys("Jenkins Selenium Python")
-    search_box.submit()
-    assert "Jenkins" in driver.page_source
     driver.quit()
+
